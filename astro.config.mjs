bd4add3cc3fc1094511046f98883e1967ed4e256
GJ Tiquia <gershom.tiquia@gmail.com>
2023-05-28 23:00:02 +0800

    feat: Add google analytics with Partytown
    https://www.kevinzunigacuellar.com/blog/google-analytics-in-astro/
    
    checked Google Analytics dashboard and confirmed it works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
